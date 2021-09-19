1ba69e0a22c5c8e4a4f24918405c6729163a7bf6
Danny Tsai <danny@saru.moe>
2021-09-19 13:55:01 +0800

    Support channel which use custom url
    There's no channel id for channels using custom url. Thus, get channel id from ytInitialData instead.
    Also,because of path of custom url starting with /c/ instead of /channel/, patched the matching rule.

--- add-members-only-videos-list-button.js
+++ add-members-only-videos-list-button.js
@@ -37,14 +37,15 @@
             const target = document.querySelector("tp-yt-paper-tab:nth-last-of-type(3)");
 
             target.addEventListener('click', function() {
-                let targetURL = `${location.protocol}//${location.host}/playlist?list=${location.pathname.split('/')[2].replace(/^UC/, 'UUMO')}`;
+                const listId = window.ytInitialData.header.c4TabbedHeaderRenderer.channelId.replace(/^UC/, 'UUMO');
+                const targetURL = `${location.protocol}//${location.host}/playlist?list=${listId}`;
                 window.open(targetURL);
             });
         }
 
         let targetURL;
         let href = location.href;
-        if (/\/channel\//.test(href)) {
+        if (/\/\/[^\/]+\/c(hannel)?\//.test(href)) {
             addLink();
         } else {
             if (window.MutationObserver) {
